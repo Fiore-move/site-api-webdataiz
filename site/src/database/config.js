de711fd76b0133f3dbb264cf7811f37014b5331f
Torres5271 <gustavo.souza@sptech.school>
2023-05-26 13:38:33 -0300

    ajuste tela de login

--- site/src/database/config.js
+++ site/src/database/config.js
@@ -21,8 +21,8 @@ var sqlServerConfig = {
 var mySqlConfig = {
     host: "localhost",
     database: "FioreMove",
-    user: "root",
-    password: "Thy08021104",
+    user: "aluno",
+    password: "sptech",
 };
 
 function executar(instrucao) {
